eba2364af1a22c63aadd28e5db572b7d23cf11bd
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-13 21:59:21 +0300

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,30 +1,64 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+console.log(galleryItems)
 
 // Створюємо шаблонний рядочок!
-const galleryOfImg = ({ preview, description }) => `<img src="${preview}" alt="${description}" class = "gallery__image">`;
+const galleryOfImg = ({ preview, original, description }) =>
+    `    
+   <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+
+
+
+
+
 // Створюємо акумулятор
 const galleryMarkup = galleryItems.reduce((acc, img) => acc + galleryOfImg(img), "");
 //Шукаємо  class = gallery
 const galleryList = document.querySelector('.gallery');
 //Додаємо шаблоні рядки
 galleryList.insertAdjacentHTML("afterbegin", galleryMarkup);
-
-
 //Делегування кліків
 const gallery = document.querySelector('.gallery');
 gallery.addEventListener('click', onclick);
 
 //Блокування вільної зони DIV
 function onclick(event) {
+    
     if (event.target.nodeName !== 'IMG') {
         return;
     }
-    event.target = galleryOfImg = ({ original, description }) => `<img src="${original}" alt="${description}" class = "gallery__image">`;
-    console.log(event.target.nodeName )
-}
+    event.preventDefault();
+      basicLightbox.create(`
+        <img src="${event.target.dataset.source}" width="800" height="600">
+     `).show()
+};
+    
+     
+
+
+ 
+
+
+
+
+
+//  `<a class="gallery__link" href = "${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"
+//     /></a>`;
 
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,36 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Створюємо шаблонний рядочок!
+const galleryOfImg = ({ preview, original, description }) =>
+    `<a class="gallery__item" href="${original}">
+  <img
+  class="gallery__image"
+  data-source="${original}" 
+  src="${preview}" 
+  alt="${description}" />
+</a>`
+
+// Створюємо акумулятор
+const galleryMarkup = galleryItems.reduce((acc, img) => acc + galleryOfImg(img), "");
+//Шукаємо  class = gallery
+const galleryList = document.querySelector('.gallery');
+//Додаємо шаблоні рядки
+galleryList.insertAdjacentHTML("afterbegin", galleryMarkup);
+//Делегування кліків
+const gallery = document.querySelector('.gallery');
+gallery.addEventListener('click', onclick);
+
+//Блокування вільної зони DIV
+  const galleryImg = new SimpleLightbox(".gallery a", {
+    //captionType : "alt",
+    captionsData: "alt",
+   // docClose: false,
+   // close : false,
+    captionDelay: 250,
+    animationSpeed : 200,
+    maxZoom: 5,
+   // disableScroll : false,
+});
+    
