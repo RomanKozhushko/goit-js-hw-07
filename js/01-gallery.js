654825f79d3b3a0c12f7d9b69f91db73942e2c73
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-13 12:17:58 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,12 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Створюємо шаблонний рядочок!
+const galleryOfImg = ({ preview }) => `<li><img src="${preview}"><class = gallery__item gallery__image:hover ></li>`;
+// Створюємо акумулятор
+const galleryMarkup = galleryItems.reduce((acc, img) => acc + galleryOfImg(img), "");
+//Шукаємо  class = gallery
+const galleryList = document.querySelector(".gallery");
+//Додаємо шаблоні рядки
+galleryList.insertAdjacentHTML("afterbegin", galleryMarkup);
