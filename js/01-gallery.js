63b7bf47d7eae59f460dc1faa3cda71fc295a45d
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-13 12:23:38 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,7 +4,7 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 // Створюємо шаблонний рядочок!
-const galleryOfImg = ({ preview }) => `<li><img src="${preview}"><class = gallery__item gallery__image:hover ></li>`;
+const galleryOfImg = ({ preview }) => `<li><img src="${preview}"><class = gallery__image></li>`;
 // Створюємо акумулятор
 const galleryMarkup = galleryItems.reduce((acc, img) => acc + galleryOfImg(img), "");
 //Шукаємо  class = gallery
