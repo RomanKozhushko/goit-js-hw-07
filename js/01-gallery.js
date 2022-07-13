9be57f518cae9d1043ee8b556c7bb708982986bc
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-13 14:16:31 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,13 +4,27 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 // Створюємо шаблонний рядочок!
-const galleryOfImg = ({ preview }) => `<img src="${preview}" class = gallery__image>`;
+const galleryOfImg = ({ preview, description }) => `<img src="${preview}" alt="${description}" class = "gallery__image">`;
 // Створюємо акумулятор
 const galleryMarkup = galleryItems.reduce((acc, img) => acc + galleryOfImg(img), "");
 //Шукаємо  class = gallery
-const galleryList = document.querySelector(".gallery");
+const galleryList = document.querySelector('.gallery');
 //Додаємо шаблоні рядки
 galleryList.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+
+//Делегування кліків
+const gallery = document.querySelector('.gallery');
+gallery.addEventListener('click', onclick);
+
+//Блокування вільної зони DIV
+function onclick(event) {
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    event.target = galleryOfImg = ({ original, description }) => `<img src="${original}" alt="${description}" class = "gallery__image">`;
+    console.log(event.target.nodeName )
+}
 
 
 
