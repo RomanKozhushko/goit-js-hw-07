e0b5009447921ccb0b8fb0dc18e889c40f50d0ac
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-14 20:45:14 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -10,7 +10,7 @@ gallery.insertAdjacentHTML("beforeend",imagesList);
 function createGallery(images) {
     return galleryItems.map(({preview, original, description }) => {
     return` <a class="gallery__item"
-        href="${original}">
+    href="${original}">
     <img class="gallery__image" 
     src="${preview}" 
     alt="${description}" />
