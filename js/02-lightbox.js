b39fbb03543f0d4ef4ed8e61e33b39d17e23345d
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-07-14 20:43:40 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,23 +8,23 @@ const imagesList = createGallery(galleryItems);
 gallery.insertAdjacentHTML("beforeend",imagesList);
 
 function createGallery(images) {
-  return galleryItems.map(({preview, original, description }) => {
-   return` <a class="gallery__item"
-    href="${original}">
-  <img class="gallery__image" 
-  src="${preview}" 
-  alt="${description}" />
-</a>`;})
-.join("")
-  }
+    return galleryItems.map(({preview, original, description }) => {
+    return` <a class="gallery__item"
+        href="${original}">
+    <img class="gallery__image" 
+    src="${preview}" 
+    alt="${description}" />
+    </a>`;})
+    .join("")
+    }
 
-  const galleryIcon = new SimpleLightbox(".gallery a", {
-        captionsData: "alt",
-   // docClose: false,
-   // close : false,
-    captionDelay: 250,
-    animationSpeed : 200,
-    maxZoom: 5,
-   // disableScroll : false,
+    const galleryIcon = new SimpleLightbox(".gallery a", {
+            captionsData: "alt",
+    // docClose: false,
+    // close : false,
+        captionDelay: 250,
+        animationSpeed : 200,
+        maxZoom: 5,
+    // disableScroll : false,
     
   });
